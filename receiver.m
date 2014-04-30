d1e7efd73795dc7ffe01150d4be6cfde0d2f666e
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-04-30 19:50:00 +0200

    Add a function to know the white percentage in grayscale image matrix.

--- receiver.m
+++ receiver.m
@@ -5,5 +5,17 @@
 %
 function message = receiver()
 
+
+
+
+%Test should return 0.2222
+whitePercentage([0.2 0.3 0.1, 0.5 0.6 0.1, 0.0 0.0 0.2])
+
 message = 'End of the receiver function.';
+end
+
+% In a grayscale image 0.0 = black and 1.0 = white
+function percentage = whitePercentage (grayImg);
+
+percentage = mean(grayImg); 
 end
