83fac962086a643760ac1f1149339c629e4c88ed
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-04-30 19:38:54 +0200

    Creation of the receiver function.

---
+++ receiver.m
@@ -0,0 +1,9 @@
+% Authors: Mickael Misbach, Lucas Vandroux 
+% License: Please refer to the LICENCE file
+% Date: April 2014
+% Version: 1
+%
+function message = receiver()
+
+message = 'End of the receiver function.';
+end
