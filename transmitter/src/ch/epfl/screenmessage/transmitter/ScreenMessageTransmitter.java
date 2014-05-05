8744e089cabc823686e3a38573755bf33b0cfef7
Mickaël Misbach <mickael.misbach@epfl.ch>
2014-05-05 16:40:48 +0200

    ajout structure de fichier projet java

---
+++ transmitter/src/ch/epfl/screenmessage/transmitter/ScreenMessageTransmitter.java
@@ -0,0 +1,10 @@
+package ch.epfl.screenmessage.transmitter;
+
+public class ScreenMessageTransmitter {
+
+	public static void main(String[] args) {
+		// TODO Stub de la méthode généré automatiquement
+
+	}
+
+}
