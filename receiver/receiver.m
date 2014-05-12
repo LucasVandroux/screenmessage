a689b42887fbc7183770b0e17a34e440fb249ae2
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-05-12 15:59:55 +0200

    function to localize the 3 Finder Pattern barely finish. Need to had other check to make it more robust.

--- receiver/receiver.m
+++ receiver/receiver.m
@@ -7,19 +7,23 @@ function message = receiver()
 
 % TODO----Webcam Capture----
 
+
 % Image process the picture to return a black and white picture.
     %Get the frame
     frame = imread('testimg1.jpg');
     % Turn it black and white 
-    %TODO-----Find a way to have a good theresold
-    frame_BW = im2bw(frame, 0.43);
+    %TODO-----Find a way to have a good theresold (try multiple one for the same picture)
+    %frame_BW = im2bw(frame, 0.43);
+    frame_BW = im2bw(frame, 0.40);
     imwrite(frame_BW, 'test.png');
     
     % TEST----Display the original frame and the BW one.
-    subplot(1,2,1), imshow(frame);
-    subplot(1,2,2), imshow(frame_BW);
+     subplot(1,2,1), imshow(frame);
+     subplot(1,2,2), imshow(frame_BW);
     
-    findFinderPattern(frame_BW(600,:),10)
+    %
+    %findFinderPattern(frame_BW(600,:),10)
+    findPositionFinderPattern(frame_BW, 20, 7)
 
 % Finding the finder pattern 
     
@@ -38,6 +42,48 @@ function message = receiver()
 message = 'End of the receiver function.';
 end
 
+% Analyse an image to find the position of the center of the 3 Finder
+% Pattern of the QRcode.
+% Input: frame = a black and white image
+%        step = integer to indicate the distance between each line which
+%               are checked for Finder Pattern.
+%        error = integer representing the variation of length autorized
+% Output: FP_Position = a matrix containing in each row the coordinates of
+%                       the center of a Finder Pattern
+% TODO---Also give the distance from the webcam
+function FP_Position = findPositionFinderPattern(frame, step, error)
+    FP_Position = [];
+    cut_range = 10;
+    j = 1;
+    while j < floor(size(frame, 1)/step)
+        row = j * step;
+        centers = findFinderPattern(frame(row,:), error);
+        if ~isempty(centers) % If not empty
+            for i = 1:size(centers, 2)
+                % We restrict a vertical line to check if it's a Finder Pattern 
+                start_FP = ceil(row - cut_range * centers(2, i));
+                end_FP = floor(row + cut_range * centers(2, i));
+                verticalFrame = transpose (frame(start_FP:end_FP, centers(1, i)));
+                center = findFinderPattern(verticalFrame, error);
+                %TODO----Test 3 other possibility to avoid problem and
+                %noise
+                if ~isempty(center) %If not empty
+                    row_position = center(1, 1) + start_FP;
+                    FP_Position = [FP_Position [row_position; centers(1, i); centers(2, i)]];
+                    j = j + cut_range;
+                    continue
+                end
+            end
+        end
+        j = j + 1;
+    end
+end
+
+% Analyse a line to find the specific pattern for Finder Pattern
+% Input: lineFrame = horizontal vector of black or white pixel
+%        error = integer representing the variation of length autorized
+% Output: centers = vector of the index of the center of all the patterns
+%                   found in the line and the unit dimension.
 function centers = findFinderPattern(lineFrame, error)
     centers = [];
     spacesBW = [lineFrame(1) ; 1];
@@ -68,7 +114,7 @@ function centers = findFinderPattern(lineFrame, error)
             end
             
             if abs(lW2 - lB1) <= error && abs(lB3 - (3*lB1)) <= error && abs(lW4 - lB1) <= error && abs(lB5 - lB1) <= error && ((lW6 + error) - lB1) >= 0
-                centers = [centers (sum(spacesBW(2, 1:i+2)) + ceil(lB3 / 2))];
+                centers = [centers [(sum(spacesBW(2, 1:i+2)) + ceil(lB3 / 2)); mean([lB1 lW2 lW4 lB5])]];
             end
             
             i = i + 1;
