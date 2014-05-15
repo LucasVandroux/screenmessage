fe96ae4d5c9d5b7b92e10c3c2ce78822464f88ad
Mickaël Misbach <mickael.misbach@epfl.ch>
2014-05-16 00:52:34 +0200

    add transmitter interface, main class

---
+++ transmitter/src/ch/epfl/screenmessage/transmitter/ASCIITransmitter.java
@@ -0,0 +1,24 @@
+package ch.epfl.screenmessage.transmitter;
+
+/**
+ * Defines a transmitter of strings, encoded in ASCII.
+ * 
+ * @author Lucas Vandroux
+ * @author Mickaël Misbach
+ */
+public interface ASCIITransmitter {
+	
+	/**
+	 * Sets the message to be transmitted.
+	 * 
+	 * @param message to be transmitted
+	 */
+	public void setMessage(String message);
+	
+	/**
+	 * Transmits the message.
+	 * 
+	 * @throws Exception if transmitting fails
+	 */
+	public void transmit() throws Exception;
+}
--- transmitter/src/ch/epfl/screenmessage/transmitter/ScreenMessageTransmitter.java
+++ transmitter/src/ch/epfl/screenmessage/transmitter/ScreenMessageTransmitter.java
@@ -4,18 +4,39 @@ public class ScreenMessageTransmitter {
 	
 	public final static String APP_NAME = "ScreenMessage Transmitter";
 	public final static int NMBR_SQUARES_BORDER = 3;
+	private final static boolean TESTING = true;
 	
-	public static void main(String[] args) {
-		TransmitterFrame f = new TransmitterFrame();
-		f.validate();
-		System.out.println("kikoo");
-		
-		for (int i = 0 ; i < 200 ; i++) {
-			char c = (char) i;
-			int v = (int) c;
-			System.out.println(c +" - "+v);
-			
+	/**
+	 * Usage : call with 2 arguments
+	 * 
+	 * @param args first argument = # of milliseconds of initial delay ; second argument = message
+	 * 
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		if (TESTING) {
+			tests();
+			return;
 		}
+		
+		if (args == null || args.length != 2) {
+			throw new IllegalArgumentException();
+		}
+		
+		Thread.sleep(Integer.parseInt(args[0]));
+		ASCIITransmitter transmitter = new Transmitter33px();
+		transmitter.setMessage(args[1]);
+		transmitter.transmit();
 	}
+	
+	private static void tests() throws Exception {
+		ASCIITransmitter transmitter = new Transmitter33px();
+		transmitter.setMessage("?????????????????????????????????????????????????????????????????????????????????????????");
+		// ? = 63 - 00111111
+		
+		transmitter.transmit();
+
+	}
+	
 
 }
