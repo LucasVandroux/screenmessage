38eae537716c7919f7418ce525c087297bfc8d7f
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-05-19 12:14:27 +0200

    Prepare the function to read the QRcode.

---
+++ receiver/readQRcode.m
@@ -0,0 +1,11 @@
+function msg = readQRcode(frame_BW, finderPatterns_pos, marge)
+%Read a QRcode once it is found in a picture
+%   Input: frame_BW = the frame where the QRcode is in Black and white
+%          finderPattern_pos = the position of the 3 finder patterns
+%          marge = marge to crop the QRcode around the finder patterns
+%   Output: msg = message bit after bit
+
+
+
+
+end
