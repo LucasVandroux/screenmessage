4b09e0051793c1d1e9988db63171f24ddba7cd4f
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-05-20 10:50:33 +0200

    Clean the code.

--- receiver/receiver.m
+++ receiver/receiver.m
@@ -39,7 +39,7 @@ function message = receiver()
     disp(['Waiting for the first QRcode...']);
     while finished == 0
         % frame = snapshot(cam);
-        frame = imread(path_img2);
+        frame = imread(path_img1);
         % Look for the first QRcode
         if reading == 0
             % Test different thereshold to find the best
@@ -64,43 +64,6 @@ function message = receiver()
         end
        
     end
-   
-    
-
-
-% Image process the picture to return a black and white picture.
-    %Get the frame
-    % frame = imread('message1-1-test.png');
-    % frame = imread('Hello_World-test.jpg');
-    % frame = imread('testimg1.jpg');
-    % frame = imread('message1-1.jpg');
-    
-    
-    % Turn it black and white 
-    %TODO-----Find a way to have a good theresold (try multiple one for the same picture)
-    %frame_BW = im2bw(frame, 0.43);
-    %frame_BW = im2bw(frame, 0.50);
-    %imwrite(frame_BW, 'test.png');
-    
-    % TEST----Display the original frame and the BW one.
-    %subplot(1,2,1), imshow(frame);
-    %subplot(1,2,2), imshow(frame_BW);     
-    
-    %imshow(getBitsPosition(frame_BW, 20, 10, 10));
-
-% Finding the finder pattern 
-    
-% TODO----Waiting for the initial_img----
-
-% TODO----When the initial_img detected find the roi----
-
-    % TODO----Process the frame to extract the roi----
-
-    % TODO----Translate the roi into a bit sequence----
-
-    % TODO----Process the bit sequence to output the message----
-    
-    % End
 
 message = 'End of the receiver function.';
 end
