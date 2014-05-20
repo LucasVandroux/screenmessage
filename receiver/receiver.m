8f1575faec010a211bbde2fffb4f1991993a602a
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-05-20 16:28:47 +0200

    Display the webcam before finding the first QRcode to help find it.

--- receiver/receiver.m
+++ receiver/receiver.m
@@ -9,6 +9,11 @@ function message = receiver()
     path_img2 = sprintf('message1-1-test.png');
     path_img3 = sprintf('Hello_World-test.jpg');
     path_img4 = sprintf('testimg1.jpg');
+    path_img5 = sprintf('test-hello-world.png');
+    path_img6 = sprintf('qrc-test-1.png');
+    path_img7 = sprintf('qrc-test-2.png');
+    path_img8 = sprintf('qrc4-test-2.png');
+    path_img9 = sprintf('qrc-test-3.png');
 
     % -----Webcam Initialization-----
     % Initialization of the object to contain the webcam
@@ -38,17 +43,19 @@ function message = receiver()
     
     disp(['Waiting for the first QRcode...']);
     while finished == 0
-        % frame = snapshot(cam);
-        frame = imread(path_img1);
+        frame = snapshot(cam);
+%         frame = imread(path_img9);
         % Look for the first QRcode
         if reading == 0
+            imshow(frame);
             % Test different thereshold to find the best
             for i = 1:size(list_thereshold_BW,1)
                 thereshold_BW = list_thereshold_BW(i);
                 frame_BW = im2bw(frame, thereshold_BW);
                 finderPatterns_pos = findPositionFinderPattern(frame_BW, step, error_max, unit_min);
+%                 finderPatterns_pos = findPositionFinderPattern4(frame_BW, step, error_max, unit_min);
                 
-                if size(finderPatterns_pos,1) == 3
+                if ~isempty(finderPatterns_pos)
                     reading = 1;
                     disp(sprintf('QRcode detected with thereshold %s.', thereshold_BW));
                     break % Get out of the for function
@@ -60,11 +67,10 @@ function message = receiver()
         if reading == 1
             frame_BW = im2bw(frame, thereshold_BW);
             msg = readQRcode(frame_BW, finderPatterns_pos, marge, error_max, step, unit_min);
+            msg_str = msg
             message = decodeMsg (msg);
             finished = 1;
         end
        
     end
-
-message = 'End of the receiver function.';
 end
