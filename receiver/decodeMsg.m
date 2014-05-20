4dc2d1a060814be6881c6ae1ca03dbfd50f55ea7
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-05-21 01:29:29 +0200

    Core of the function finished. Prepare all what is needed to code the checksum.

--- receiver/decodeMsg.m
+++ receiver/decodeMsg.m
@@ -1,7 +1,8 @@
 function msg_text = decodeMsg(msg)
 %DECODEMSG Summary of this function goes here
 %   Detailed explanation goes here
-msg_text = bits2text(msg(49:760));
+msg_to_traduct = msg(49:end)
+msg_text = bits2text(msg(49:end));
 
 end
 
