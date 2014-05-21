64a03c2887ce3cf5b4b5237f02876c13958f95ac
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-05-21 02:44:12 +0200

    Finish to implement a new algorithm to make the function more efficient.

--- receiver/readQRcode4.m
+++ receiver/readQRcode4.m
@@ -21,20 +21,15 @@ function msg = readQRcode4(frame_BW, finderPatterns_pos, marge, error_max, step,
     
     % Check if we were able to find the QRcode in the given dimensions
     if ~isempty(specific_finderPattern_pos)
-        unit = floor(mean(specific_finderPattern_pos(:,3)));
-
-        % Get the position of the lines
-        rowcol_pos = findRowColPosition(QRcode, specific_finderPattern_pos, unit, error_max);
-
         % Read the horizontal and vertical lines
-        msg = readLines(QRcode, specific_finderPattern_pos, unit, rowcol_pos);
+        msg = readLines(QRcode, specific_finderPattern_pos);
 
     else % Return an empty line to alert that the position of the QRcode in the frame have to be compute again
         msg = [];
     end
 end
 
-function msg_bits_str = readLines(QRcode, FP_pos, unit, rowcol_pos)
+function msg_bits_str = readLines(QRcode, FP_pos)
 %Read a the horizontal and vertical line of a given QRcode
 %   Input: QRcode = the QRcode cropped and with the perspective adjusted
 %          finderPattern_pos = the position of the 4 finder patterns
@@ -42,8 +37,8 @@ function msg_bits_str = readLines(QRcode, FP_pos, unit, rowcol_pos)
 %          rowcol_pos = position of all the limits beetween the lines
 %   Output: msg_bits_str = string containing all the bits of the message
     
-    step = 17;
-    
+    % Initialize the variable
+    step = 17; 
     unit_v = ceil((FP_pos(3,2) - FP_pos(1,2))/26);
     unit_h = ceil((FP_pos(2,1) - FP_pos(1,1))/26);
     
@@ -51,14 +46,10 @@ function msg_bits_str = readLines(QRcode, FP_pos, unit, rowcol_pos)
     x_start = floor(FP_pos(1,1) - 3.5 * unit_h);
     x_stop = ceil(FP_pos(2,1) + 3.5 * unit_h);
     
+    % Crop and rescale the region of interest for the horizontal lines
     QRcode_hlines = QRcode(floor(FP_pos(1,2) + 4.5 * unit_v):ceil(FP_pos(3,2) - 4.5 * unit_v), x_start:x_stop);
     QRcode_hlines_rescal = imresize(QRcode_hlines, [289 NaN]); % Rescale the QRcode horizontal lines to have a multiple of 17
-    
-    
-    imshow(QRcode_hlines_rescal);
-    
-    
-    
+ 
     h_msg_bits = [];
     
     % Read all the horizontal lines
@@ -67,20 +58,13 @@ function msg_bits_str = readLines(QRcode, FP_pos, unit, rowcol_pos)
         h_msg_bits = [h_msg_bits ; readLine(line)];
     end
     
-%     % Read all the horizontal lines
-%     for i = 2:(size(rowcol_pos, 1)-2)
-%         line = QRcode(rowcol_pos(i,1):rowcol_pos(i+1,1), x_start:x_stop);
-%         h_msg_bits = [h_msg_bits ; readLine(line)];
-%     end
-    
      % Get the step and the border for each vertical line
     y_start = floor(FP_pos(1,2) - 3.5 * unit_v);
     y_stop = ceil(FP_pos(3,2) + 3.5 * unit_v);
-
+    
+    % Crop and rescale the region of interest for the vertical lines
     QRcode_vlines = QRcode(y_start:y_stop, floor(FP_pos(1,1) + 4.5 * unit_h):ceil(FP_pos(2,1) - 4.5 * unit_h));
     QRcode_vlines_rescal = imresize(QRcode_vlines, [NaN 289]); % Rescale the QRcode vertical lines to have a multiple of 17
-    
-    imshow(QRcode_vlines_rescal);
     
     v_msg_bits = [];
     
@@ -89,12 +73,6 @@ function msg_bits_str = readLines(QRcode, FP_pos, unit, rowcol_pos)
         line = transpose(QRcode_vlines_rescal(:,(1 + step*(i-1)):(step*i)));
         v_msg_bits = [v_msg_bits ; readLine(line)];
     end
-    
-%     % Read all the vertical lines
-%     for i = 2:(size(rowcol_pos, 1)-2)
-%         line = transpose(QRcode(y_start:y_stop, rowcol_pos(i,2):rowcol_pos(i+1,2)));
-%         v_msg_bits = [v_msg_bits ; readLine(line)];
-%     end
     
     % Get each part from the horizontal lines
     msg_0_to_101 = [];
@@ -136,167 +114,9 @@ function msg_line = readLine(line)
 %   Output: msg_line = bits contained in the line
     msg_line = ones(1,33);
     line_rescal = imresize(line, [NaN 363]); % Rescale the line to have a multiple of 33
-    
-    
-    
-    imshow(line_rescal);
-    
-    
     step = 11;
         
     for i = 1:33
         msg_line(1,i) = round(mean2(line_rescal(:,(1 + step*(i-1)):(step*i)))); 
     end
-end
-
-function rowcol_pos = findRowColPosition (QRcode, finderPattern_pos, unit, error_max)
-%Read a QRcode once it is found in a picture
-%   Input: QRcode = the QRcode cropped and with the perspective adjusted
-%          finderPattern_pos = the position of the 4 finder patterns
-%          unit = size representing the size of a bit of the QRcode
-%          error_max = maximum error tolerated
-%   Output: rowcol_pos = matrice containing the limits between each
-%                        horizontal and vertical lines in two column vector
-    
-    % Unit indicating the limits of the timing line from the FP's center
-    small_cst = 2.5;
-    big_cst = 3.5;
-    
-    % Getting the position of the FP needed to find the timing lines
-    x_pos_FP_lt = finderPattern_pos(1,1);
-    x_pos_FP_rt = finderPattern_pos(2,1);
-    
-    y_pos_FP_lt = finderPattern_pos(1,2);
-    y_pos_FP_lb = finderPattern_pos(3,2);
-
-    % Get the row position from the left vertical timing
-    v_left_timing = pixelColToSpaces(QRcode(:,floor(x_pos_FP_lt + small_cst*unit):ceil(x_pos_FP_lt + big_cst*unit)));
-    v_left_pos = findLinePos(v_left_timing, unit, error_max);
-    
-    % Get the row position from the right vertical timing
-    v_right_timing = pixelColToSpaces(QRcode(:,floor(x_pos_FP_rt - big_cst*unit):ceil(x_pos_FP_rt - small_cst*unit)));
-    v_right_pos = findLinePos(v_right_timing, unit, error_max);
-    
-    % Combine the two lines by computing the mean between the two
-    v_pos = [];
-    for i = 1:size(v_left_pos, 2)
-        v_pos = [v_pos ; floor((v_left_pos(1,i) + v_right_pos(1,i)) / 2)];
-    end
-    
-    % Get the row position from the top horizontal timing
-    h_top_timing = pixelColToSpaces(transpose(QRcode(floor(y_pos_FP_lt + small_cst*unit):ceil(y_pos_FP_lt + big_cst*unit),:)));
-    h_top_pos = findLinePos(h_top_timing, unit, error_max);
-    
-    % Get the row position from the bottom horizontal timing
-    h_bottom_timing = pixelColToSpaces(transpose(QRcode(floor(y_pos_FP_lb - big_cst*unit):ceil(y_pos_FP_lb - small_cst*unit),:)));
-    h_bottom_pos = findLinePos(h_bottom_timing, unit, error_max);
-    
-    % Combine the two lines  by computing the mean between the two
-    h_pos = [];
-    for i = 1:size(h_top_pos, 2)
-        h_pos = [h_pos ; floor((h_top_pos(1,i) + h_bottom_pos(1,i)) / 2)];
-    end
-    
-    % Combine the two column vector
-    rowcol_pos = [v_pos h_pos];
-end
-
-function line_pos = findLinePos (spaces, unit, error_max)
-%Return the top of each line in the spaces given
-%   Input: spaces = vector of color and size of the space of it
-%          error_max = error tolerate
-%          unit = unit of a pixel in the QRcode
-%   Output: line_pos = vector containing the position of the limit between
-%           each line
-
-    % Initialize variables
-    size_timing = 19;
-    line_pos = [];
-    
-    function index_FP = findSideFinderPattern (index_start, spaces, error_max, unit)
-    %Return the index of the first finder pattern side in the spaces
-    %   Input: index_start = index where to start looking for the finder
-    %                        pattern
-    %          spaces = vector of color and size of the space of it
-    %          error = error tolerate
-    %          unit = unit of a pixel in the QRcode
-    %   Output: index_FP = index of the first FP's side founded
-        
-        % Initialize variables
-        index = index_start;
-        index_FP = 0;
-        
-        while index_FP == 0
-            % Check if the current space correspond to the finder pattern side
-            if spaces(1, index) == 0 && abs(spaces(2, index) - (7 * unit)) <= 4 * error_max
-                index_FP = index;
-            end
-            
-            index = index + 1; 
-            
-            % Put the index_FP to -1 if there is not side finder patter in
-            % the spaces
-            if index > size(spaces, 2)
-                index_FP = -1;
-            end
-        end
-    end
-    
-    % Find the first side of the finder pattern
-    index_first_FP = findSideFinderPattern(1, spaces, error_max, unit);
-    
-    % Throw an error if there is no finder pattern side in the spaces
-    if index_first_FP == -1
-        imshow(spaces(1,:));
-        error('No Finder Pattern side found in the line.');
-    end
-    
-    x = 0;
-    index = index_first_FP + 1;
-
-    while x < size_timing
-        if abs(spaces(2, index) - unit) < error_max
-            % Record the position of the line up
-            line_pos = [line_pos sum(spaces(2,1:(index-1)))];
-            x = x + 1;
-            index = index + 1;
-        else    
-            % Merge the small part with the next part
-            sum_spaces = sum(spaces(2:index:index+2));
-            spaces_row1 = [spaces(1,1:index), spaces(1,(index + 3):end)];
-            spaces_row2 = [spaces(2,1:(index - 1)), sum_spaces, spaces(2,(index + 3):end)];
-            spaces = [spaces_row1; spaces_row2];
-            disp('Merge the small part');
-        end
-        
-        if x == size_timing
-            % Take the last limit
-            line_pos = [line_pos sum(spaces(2,1:(index-1)))];
-        end
-    end
-end
-
-function spacesBW = pixelColToSpaces(pixel_col)
-%Convert the pixel column in black and white spaces using the mean of each
-%lines.
-%   Input: pixel_col = column of pixels lines
-%   Output: spacesBW = matrice containing the best probable color of each line in the first
-%                      line and the size(number of lines) of the space on the second.
-
-    spacesBW = [color(pixel_col(1,:)) ; 1];
-   
-    for y=2:size(pixel_col, 1)
-        if color(pixel_col(y,:)) == spacesBW(1, end)
-            spacesBW(2, end) = spacesBW(2, end) + 1;
-        else
-            spacesBW = [spacesBW [color(pixel_col(y,:)); 1]];
-        end
-    end
-end
-
-function color = color(line)
-%Compute the color according to the mean of the color of the line
-%   Input: line = line of pixel
-%   Output: color = color of the line
-    color = round(mean(line(:)));
 end
