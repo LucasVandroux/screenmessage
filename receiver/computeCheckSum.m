763fe2ea6e9da7bce27d6ce440f14e0ce32d2441
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-05-22 08:40:52 +0200

    Separate the function to compute the checksum from the function to decode the message in order to be able to test it more easily.

---
+++ receiver/computeCheckSum.m
@@ -0,0 +1,22 @@
+function checksum = computeCheckSum(msgToCheck)
+%Compute the check sum of the input using the SDBM algorithm
+%   Input: msgToCheck = string containing all the bits of the message we
+%                       want to compute the checksum on.
+%   Output: checksum = checksum of the message
+    
+    disp(['Length message to check: ',length(msgToCheck)]);
+    % Initialize the checksum
+    checksum = 0;
+    
+    % Compute the checksum
+    for i = 1:(length(msgToCheck)/8)
+        checksum = mod(checksum * 65599 + bin2dec(msgToCheck((1+(i-1)*8) : (i*8))), 2^32-1);
+        disp(['Iteration ', num2str(i), ' ', dec2bin(checksum)]);
+    end
+    
+    % Convert the checksum in binary
+    checksum = dec2bin(checksum);
+    
+    % Casted the checksum to 32 bits
+%     checksum = checksum((end-31):end);
+end
--- receiver/decodeMsg.m
+++ receiver/decodeMsg.m
@@ -3,14 +3,11 @@ function msg_text = decodeMsg(msg)
 %   Input: msg = string containing all the bits of the message
 %   Output: msg_text = [] if the checksum is not right and the message if
 %                      it is
-
+    
+    disp(['Bits message = ', msg]);
     % Compute the index of the last bit
     data_length = bin2dec(msg(41:48));
     last_bit = 48 + (data_length*8);
-    
-    %-------------TEST-------------------
-%     msg_text = bits2text(msg(49:last_bit));
-    %-------------END-TEST-------------------
     
     % Compute the checksum of the message
     checksum = computeCheckSum(msg(33:last_bit));
@@ -20,30 +17,12 @@ function msg_text = decodeMsg(msg)
         % Decode the message from bits to text
         msg_text = bits2text(msg(49:last_bit));
     else
+        %-------------TEST-------------------
+        disp(['Wrong Checksum = ', checksum, ' Expected = ', msg(1:32), ' Message = ', bits2text(msg(49:last_bit))]);
+        %-------------END-TEST-------------------
+    
         msg_text = [];
     end
     
-end
-
-function checksum = computeCheckSum(msgToCheck)
-%Compute the check sum of the input using the SDBM algorithm
-%   Input: msgToCheck = string containing all the bits of the message we
-%                       want to compute the checksum on.
-%   Output: checksum = checksum of the message
-
-    % Initialize the checksum
-    checksum = 0;
-    
-    % Compute the checksum
-    for i = 1:(length(msgToCheck)/8)
-        checksum = mod(checksum * 65599 + bin2dec(msgToCheck((1+(i-1)*8) : (i*8))), 2^32-1);
-    end
-    
-    % Convert the checksum in binary
-    checksum = dec2bin(checksum);
-    
-    % Casted the checksum to 32 bits
-    checksum = checksum((end-31):end);
-
 end
 
