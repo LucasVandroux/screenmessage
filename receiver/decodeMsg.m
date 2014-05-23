938fb5807a98096842af774a85b4a86be108bf87
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-05-23 15:24:14 +0200

    Correct the bug to verify the computed checksum with the decoded checksum.

--- receiver/decodeMsg.m
+++ receiver/decodeMsg.m
@@ -3,8 +3,7 @@ function msg_text = decodeMsg(msg)
 %   Input: msg = string containing all the bits of the message
 %   Output: msg_text = [] if the checksum is not right and the message if
 %                      it is
-    
-    disp(['Bits message = ', msg]);
+
     % Compute the index of the last bit
     data_length = bin2dec(msg(41:48));
     last_bit = 48 + (data_length*8);
@@ -13,14 +12,14 @@ function msg_text = decodeMsg(msg)
     checksum = computeCheckSum(msg(33:last_bit));
 
     % Compare it with the one in the message
-    if strcmp(checksum,msg(1:32))    
+    if bin2dec(checksum) == bin2dec(msg(1:32))    
         % Decode the message from bits to text
         msg_text = bits2text(msg(49:last_bit));
     else
-        %-------------TEST-------------------
-        disp(['Wrong Checksum = ', checksum, ' Expected = ', msg(1:32), ' Message = ', bits2text(msg(49:last_bit))]);
-        %-------------END-TEST-------------------
-    
+        % Display a message if the checksum is wrong
+        % disp(['/!\ -- Wrong Checksum = ', checksum, ' Expected = ', msg(1:32), ' Message = ', bits2text(msg(49:last_bit))]);
+        
+        % Return an empty message
         msg_text = [];
     end
     
